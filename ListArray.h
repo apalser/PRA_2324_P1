9f988dc3fb09e832cb65da20193b2fad506a73f5
Angel Palacios Server <apalser@etsinf.upv.es>
2024-11-12 23:28:04 +0100

    Añadir ListArray.h

--- ListArray.h
+++ ListArray.h
@@ -15,7 +15,7 @@ class ListArray : public List<T> {
 		~ListArray();
 		T operator[](int pos);
 		friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list) {
-		      out << "List: [" << std::endl;
+		      out << "List => [" << std::endl;
 			if(list.n != 0) {
 				for(int i = 0; i < list.n; i++) {
 					out << list.arr[i] << std::endl;
@@ -35,3 +35,120 @@ class ListArray : public List<T> {
 		int size() override;
 
 };
+
+template<typename T>
+ListArray<T>::ListArray() : max{MINSIZE}, n{0} {
+	arr = new T[MINSIZE];
+	for(int i = 0; i < MINSIZE; i++){
+		arr[i] = -1;
+	}	 
+} 
+template<typename T>
+ListArray<T>::~ListArray() {
+	delete[] arr;
+}
+template<typename T>
+T ListArray<T>::operator[](int pos) {
+	if(pos > (-1) && pos < max){ 
+		return arr[pos];
+	}else{
+		throw std::out_of_range("Posición inválida");
+	}
+}
+void ListArray<T>::resize(int new_size) {
+	T* nuevoArr = new T[new_size];
+	int i = 0;
+	while(i < n) {
+		nuevoArr[i] = arr[i];
+		i++;
+	}
+	delete[] arr;
+	while(i < new_size) {
+		nuevoArr[i] = -1;
+		i++;
+	}
+	arr = nuevoArr;
+	max = new_size;
+}
+template<typename T>
+void ListArray<T>::insert(int pos, T e) {
+	if(pos < 0 || pos > n){
+		throw std::out_of_range("Posición inválida");
+	}else if(pos == n){
+		arr[pos] = e;
+	}else{
+		T aux;
+		while(pos < n){
+			aux = arr[pos];
+			arr[pos] = e;
+			e = aux;
+			pos++;
+		}
+	}
+	n++;
+	if(n == max){
+		resize(max + 10);
+	}
+}
+
+template<typename T>
+void ListArray<T>::append(T e) {
+	insert(n, e);
+}
+template<typename T>
+void ListArray<T>::prepend(T e) {
+	insert(0, e);
+}
+template<typename T>
+T ListArray<T>::remove(int pos) {
+	T aux = arr[pos];
+	if(pos < 0 || pos > (n-1)) {
+		throw std::out_of_range("Posición inválida!!");
+	}else if(pos == (n - 1)){
+		arr[pos] = -1;
+	}else{ 
+		while(pos != n){
+			arr[pos] = arr[pos+1];
+			pos++;
+		}
+															}
+	n--;
+	if((max - n) > 10){ 
+		resize(max - 10);
+	}
+	return aux;
+}
+template<typename T>
+T ListArray<T>::get(int pos) {
+	if(pos < (-1) || pos > (n - 1)){
+		throw std::out_of_range("Posición inválida");
+	}
+	return arr[pos];
+}
+template<typename T>
+int ListArray<T>::search(T e) {
+	for(int i = 0; i < n; i++) {
+		if(arr[i] == e){
+			return i;
+		}
+	}
+	return -1;
+}
+template<typename T>
+bool ListArray<T>::empty() {
+	if(n == 0){ 
+		return true;
+	}else{
+		return false;
+}
+template<typename T>
+int ListArray<T>::size() {
+	return n;
+}
+
+
+
+
+
+
+
